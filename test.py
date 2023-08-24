481cce2cdf5544c032bdf19a0b3472b64ba090e1
Thomas Roberts <thomas_roberts1@baylor.edu>
2023-08-24 15:13:42 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c
+
+message = "Hello World!"
+
+print(message)
+
+#breakpoints pause the code at that line and do not execute that line.
+#use debug console to test variables and difference scenarios
+
